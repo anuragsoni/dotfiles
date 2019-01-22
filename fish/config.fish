eff059cbbae82a02404ad256eda5c19e1645edb0
Anurag Soni <anurag@sonianurag.com>
2019-01-22 08:27:58 -0500

    add comment for WSL

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 # opam configuration
 source /home/asoni/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 source ~/.asdf/asdf.fish
+
+# umask 22 needed when running this on Windows Subsystem for Linux
+# umask 22
